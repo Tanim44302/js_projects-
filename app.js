6e4d8953a48c5407ecbfb92d79633d12c479334c
Tanim mohammad <tanimmohammad100@gmail.com>
2024-09-07 01:30:27 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
-const sidebarBtn = document.querySelector(`nav .sidebarBtn`)
-const sidebar = document.querySelector(`.sidebar`)
-const cancelBtn = document.querySelector(`.cancel`)
+ const darkBtn = document.querySelector(".darkBtn") ;
+ const body = document.querySelector("body");
 
+ const darkMode = () => body.classList.toggle("darkMode");
 
-const openSidebar = () => {
-    sidebar.classList.add(`active`)
-
-}
-
-
-sidebarBtn.addEventListener('click', openSidebar)
-
-
-const closeSidebar = (event) => {
-
-    const {target} = event
-    if((target.classList.contains(`sidebar`) || target.classList.contains(`cancel`)) ){
-
-        sidebar.classList.remove(`active`)
-    }
-
-}
+ darkBtn.addEventListener("click", darkMode);
 
 
 
-cancelBtn.addEventListener(`click`, closeSidebar)
 
-sidebar.addEventListener(`click`, closeSidebar)
+
+
+
+ const galleryPopup = document.querySelector(".popupGallery");
+  const galleryImages = Array.from(document.querySelectorAll(".gallImage")); 
+  const galleryPopupImage = galleryPopup.querySelector("img"); 
+  let currentPosition = 0;
+   // console.log(galleryImages); 
+   const openGallery = (event, index) => {  
+     currentPosition = index; 
+      const image = event.currentTarget.src; 
+    galleryPopupImage.src = image; 
+   galleryPopup.classList.add("active");
+   }
+         
+    galleryImages.map((gallImg, index) => { 
+     gallImg.addEventListener("click", () => openGallery(event, index));
+     });
+     const cancelBtn = document.querySelector(".cancelBtn");
+      const closeGallery = () => galleryPopup.classList.remove("active");
+     cancelBtn.addEventListener("click", closeGallery);
+      const nextArrow = document.querySelector(".rightArrow");
+       const prevArrow = document.querySelector(".leftArrow");
+     const galleryNext = () => {
+           if (currentPosition == galleryImages.length - 1) {
+                 currentPosition = 0;
+                   } else {     currentPosition += 1;
+
+                      }
+                      
+                      galleryPopupImage.src = galleryImages[currentPosition].src;
+                     }
+                      const galleryPrev = () => {
+                           if (currentPosition == 0) {
+                                 currentPosition = galleryImages.length - 1;
+                                   }
+                                   
+                                   else {     currentPosition -= 1;
+
+                                      }
+                                      
+                                      galleryPopupImage.src = galleryImages[currentPosition].src;
+                                     }
+                                     
+                                     nextArrow.addEventListener("click", galleryNext);
+                                      prevArrow.addEventListener("click", galleryPrev);
+
+
+
+  
